603bdc336981c8fa12a4aa4a5602801c374f50c3
Greg Bell <greg@bitwombat.com.au>
2018-09-14 13:57:46 +1000

    Refactor to be closer to existing tests

--- tests/4-MultilineTest.php
+++ tests/4-MultilineTest.php
@@ -4,32 +4,40 @@ use PHPUnit\Framework\TestCase;
 
 class MultilineTest extends TestCase
 {
-    public function testConfigValues()
-    {
-        $test_config_path = __DIR__ . '/wp-config-test-update.php';
+	protected static $test_config_path;
+	protected static $config_transformer;
 
+    public static function setUpBeforeClass() {
+        self::$test_config_path = __DIR__ . '/wp-config-test-multiline.php';
         /* The // at the end of the first line causes parse_wp_config to
            have a space prefixing the second line, which causes the preg_replace
            in update to fail.
            Fixed in ad435a7
         */
-        file_put_contents( $test_config_path, <<<EOF
+        file_put_contents( self::$test_config_path, <<<EOF
 <?php
 define('WP_CACHE', true); //
 define('DB_NAME', 'oldvalue');
 EOF
 );
+        self::$config_transformer = new WPConfigTransformer( self::$test_config_path );
 
-        $config_transformer = new WPConfigTransformer( $test_config_path );
+    }
 
-        $config_transformer->update('constant', 'DB_NAME', 'newvalue');
 
-        require_once $test_config_path;
+    public static function tearDownAfterClass() {
+        unlink( self::$test_config_path );
+    }
+
+    public function testConfigValues()
+    {
+        self::$config_transformer->update('constant', 'DB_NAME', 'newvalue');
+
+        require_once self::$test_config_path;
 
         $this->assertTrue( defined( 'DB_NAME' ), 'DB_NAME not defined');
         $this->assertNotEquals( 'oldvalue', constant( 'DB_NAME' ), 'DB_NAME is still "oldvalue"' );
         $this->assertEquals( 'newvalue', constant( 'DB_NAME' ), 'DB_NAME is not "newvalue"');
 
-        unlink( $test_config_path );
     }
 }
