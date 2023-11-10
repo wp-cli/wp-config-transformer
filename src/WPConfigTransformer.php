d594226914878545978c1fb221658e5e39707bce
Alex Concha <alex@buayacorp.com>
2023-11-10 08:31:40 +0100

    Replace DOS line endings with LF
    DOS line endings are not currently stripped from the wp-config files,
    which results in extra lines being added to the modified file.
    
    Fixes https://github.com/wp-cli/config-command/issues/160

--- src/WPConfigTransformer.php
+++ src/WPConfigTransformer.php
@@ -70,7 +70,7 @@ class WPConfigTransformer {
 			throw new Exception( 'Config file is empty.' );
 		}
 		// Normalize the newline to prevent an issue coming from OSX.
-		$this->wp_config_src = str_replace( array( "\n\r", "\r" ), "\n", $wp_config_src );
+		$this->wp_config_src = str_replace( array( "\r\n", "\n\r", "\r" ), "\n", $wp_config_src );
 		$this->wp_configs    = $this->parse_wp_config( $this->wp_config_src );
 
 		if ( ! isset( $this->wp_configs[ $type ] ) ) {
